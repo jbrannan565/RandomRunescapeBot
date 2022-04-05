aa9820e387e5977feb7443f3a6f7e475148ffa68
Jared Brannan <jared.brannan@wsu.edu>
2022-04-04 19:04:04 -0700

    add channel restriction

--- cogs/fetcher.py
+++ cogs/fetcher.py
@@ -3,14 +3,18 @@ from discord.ext import commands
 from _lib.runescape_fetcher import RunscapeFetcher
 
 class Fetcher(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, channel_name):
         self.bot = bot
         self.fetcher = RunscapeFetcher()
+        self.channel_name = channel_name
 
     @commands.command(help='''
         Get a random fact from the runescape wiki.
     ''') 
     async def random(self, ctx, resource_type, *args):
+        if ctx.channel.name != self.channel_name:
+            return 
+
         text = self.fetcher.get_text()
         await ctx.send(f"Did you know that: \n{text}")
 
